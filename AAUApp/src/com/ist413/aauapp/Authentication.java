2c0a7c861974f8250d1a0d9e06479f017d5e5155
rockjolt375 <rockjolt375@hotmail.com>
2014-02-16 14:47:50 -0500

    Additions to Authentication.java

--- AAUApp/src/com/ist413/aauapp/Authentication.java
+++ AAUApp/src/com/ist413/aauapp/Authentication.java
@@ -2,11 +2,28 @@ package com.ist413.aauapp;
 
 public class Authentication {
 
+	private static String loggedUser = null;
+	private static String loggedPass = null;
+	
+	public enum AuthGroup{ADMINISTRATOR, CONTRIBUTOR, GENERAL}
+
+	public Authentication() {
+	
+	}
+	
+	public Authentication(String user, String pass){
+		
+	}
+	
+	public boolean isAuthorized(){
+		
+		return false;
+	}
+	
+	
 	/*
-	 * 
-	 * Will contain authentication and login management
-	 * for across the app utilizing our storage .json
+	 * thread for checking accounts against
+	 * json 
 	 * 
 	 */
-	
 }
